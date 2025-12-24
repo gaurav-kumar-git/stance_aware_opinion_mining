ef7d3d2121584bb04626a33715bf9e2252310645
Gaurav Kumar <83592505+gaurav-kumar-git@users.noreply.github.com>
2025-12-24 22:36:16 +0530

    Clean up config.py by removing comments
    Removed commented sections for clarity.

--- src/config.py
+++ src/config.py
@@ -4,18 +4,15 @@ LOCAL_BASE_MODEL_PATH = "/home/sslab/24m0786/stance_aware_opinion_mining/local_c
 LOCAL_TRAIN_DATA_DIR = "/home/sslab/24m0786/stance_aware_opinion_mining/local_cache/train"
 LOCAL_HF_DATASET_DIR = "/home/sslab/24m0786/stance_aware_opinion_mining/local_cache/kialo_offline_dataset"
 
-# --- Training Hyperparameters (can be left as default) ---
 TRAIN_BATCH_SIZE = 16
 EVAL_BATCH_SIZE = 32
 NUM_EPOCHS = 1
 LEARNING_RATE = 2e-5
 WARMUP_STEPS_RATIO = 0.1
 
-# triplet training specific
 MAX_TRIPLET_SAMPLES = 500000
 
 
-# --- Output Paths (can be left as default) ---
 RESULTS_DIR = "results"
 SIAMESE_MODEL_SAVE_PATH = os.path.join(RESULTS_DIR, "models", "siamese_model")
 TRIPLET_MODEL_SAVE_PATH = os.path.join(RESULTS_DIR, "models", "triplet_model")
